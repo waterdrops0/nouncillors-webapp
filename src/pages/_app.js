b181ff0df36340c5dddf156b2162f6e738627d9e
curelycue <eduard02stoica@gmail.com>
2024-10-19 16:15:08 +0300

    Add style

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { config } from "../eth/config";
 import { WagmiProvider } from "wagmi";
-import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { RainbowKitProvider, lightTheme } from "@rainbow-me/rainbowkit";
 import "@rainbow-me/rainbowkit/styles.css";
 import "../styles/globals.css";
 
@@ -16,7 +16,14 @@ function App({ Component, pageProps }) {
       </Head>
       <WagmiProvider config={config}>
         <QueryClientProvider client={queryClient}>
-          <RainbowKitProvider>
+          <RainbowKitProvider
+      theme={lightTheme({
+        overlayBlur: 'small',
+        accentColor: '#d94339',
+        accentColorForeground: 'white',
+        borderRadius: 'none',
+        fontStack: 'system',
+      })} >
             <Component {...pageProps} />
           </RainbowKitProvider>
         </QueryClientProvider>
