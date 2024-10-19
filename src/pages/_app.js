51c83b22a158f65797e71bfc7127efb8dd0cdf38
curelycue <eduard02stoica@gmail.com>
2024-10-19 23:55:40 +0300

    Move Wagmi Config to _app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,24 @@
 import Head from "next/head";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { config } from "../eth/config";
 import { WagmiProvider } from "wagmi";
 import { RainbowKitProvider, lightTheme } from "@rainbow-me/rainbowkit";
 import "@rainbow-me/rainbowkit/styles.css";
 import "../styles/globals.css";
+import { getDefaultConfig } from '@rainbow-me/rainbowkit';
+import { http } from 'wagmi';
+import { mainnet, sepolia, } from 'wagmi/chains';
+
+const config = getDefaultConfig({
+  appName: 'Join-Nouncil',
+  projectId: '30eb1d2ba9fd631ac27a190400dfb379',
+  chains: [mainnet, sepolia],
+  ssr: true, // If your dApp uses server side rendering (SSR)
+  transporters: {
+    [mainnet.id]: http('https://mainnet.infura.io/v3/1290d77fdc964638b22f2cbf80c76373'),
+    [sepolia.id]: http('https://sepolia.infura.io/v3/1290d77fdc964638b22f2cbf80c76373'),
+  }
+});
+
 
 const queryClient = new QueryClient();
 
